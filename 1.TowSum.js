c0bc1b4b75f71095acc3bd424b4429bf4005ff33
Jingxuan Chen <jean.chen@laserfiche.com>
2018-11-08 14:38:04 +0800

    1. TwoSum
    Initializing my config file as JavaScript

---
+++ 1.TowSum.js
@@ -0,0 +1,35 @@
+/**
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+**/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ 
+var twoSum = function(nums, target) {
+    if (nums == null || nums.length == 0) {
+    	return [-1, -1];
+    }
+
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+    	if (map.has(target - nums[i])) {
+    		return [map.get(target - nums[i]), i];
+    	} 
+
+    	map.set(nums[i], i);
+    }
+
+    return [-1, -1];
+};
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Leetcode-JavaScript
-This is a practice project for implementing the Leetcode problems in JavaScript. 
+### Leetcode JavaScript
+
+Well, this is a practic project for implementing the Leetcode problems in JavaScript.
+
+Starting from 2018.11.8
+
+>Any application that can be written in JavaScript, will eventually be written in JavaScript
